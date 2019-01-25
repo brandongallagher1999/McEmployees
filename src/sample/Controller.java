4ad6d033d99aedd635297f5911bf88cc8abe9ae7
cryptoguys10 <36753063+cryptoguys10@users.noreply.github.com>
2019-01-25 14:55:24 -0500

    initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller {
+
+
+
+    @FXML private Button btn_test;
+
+    public void initialize(URL url, ResourceBundle rb){
+        btn_test.
+    }
+}
